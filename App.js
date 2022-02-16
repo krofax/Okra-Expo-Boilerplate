b85a05705deb8bc5e6ec2d0edd92de647ed64ef2
Blessing Krofegha <bkrofegha@gmail.com>
2022-02-16 12:14:52 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+ import {
+   Alert,
+   SafeAreaView,
+   StatusBar,
+   StyleSheet,
+   Text,
+   useColorScheme,
+   View,
+ } from 'react-native';
+
+import { Okra } from 'react-native-okra-webview';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Okra.BuildWithOptions
+        token="5da6358130a943486f33dced"
+        okraKey="7d9d39f0-23a0-5b93-8603-6c03fb0e02c5"
+        env="production"
+        products={["auth", "identity", "balance", "transaction"]}
+        name={'tobi the builder'}
+        onSuccess={(e)=>{
+            console.log("success>>>>", e)
+        }}
+        onError={(e)=>{
+        }}
+        onClose={(e)=>{
+        }}
+      />
     </View>
   );
 }
